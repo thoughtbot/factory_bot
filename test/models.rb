47a6b731ccb7334e0ca1bf432a9d4d9b2871e008
Joe Ferris <jferris@thoughtbot.com>
2008-06-01 11:12:40 -0700

    Added the missing models file

---
+++ test/models.rb
@@ -0,0 +1,32 @@
+ActiveRecord::Base.establish_connection(
+  :adapter  => 'sqlite3',
+  :database => File.join(File.dirname(__FILE__), 'test.db')
+)
+
+class CreateSchema < ActiveRecord::Migration
+  def self.up
+    create_table :users, :force => true do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :email
+      t.boolean :admin, :default => false
+    end
+
+    create_table :posts, :force => true do |t|
+      t.string  :title
+      t.integer :author_id
+    end
+  end
+end
+
+CreateSchema.suppress_messages { CreateSchema.migrate(:up) }
+
+class User < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name, :email
+  has_many :posts, :foreign_key => 'author_id'
+end
+
+class Post < ActiveRecord::Base
+  validates_presence_of :title, :author_id
+  belongs_to :author, :class_name => 'User'
+end
