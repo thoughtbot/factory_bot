9990cbebb328476b96420ad82f97d4740f0ab127
Nick Quaranto <nick@quaran.to>
2010-01-16 15:24:20 -0500

    Moving error message out to a constant

--- lib/factory_girl/proxy/stub.rb
+++ lib/factory_girl/proxy/stub.rb
@@ -2,7 +2,9 @@ class Factory
   class Proxy
     class Stub < Proxy #:nodoc:
       @@next_id = 1000
-      
+
+      ERROR_MESSAGE = "stubbed models are not allowed to access teh database"
+
       def initialize(klass)
         @instance = klass.new
         @instance.id = next_id
@@ -12,21 +14,21 @@ class Factory
           end
 
           def connection
-            raise "stubbed models are not allowed to access the database"
+            raise ERROR_MESSAGE
           end
-          
+
           def class
             klass = super.dup
             klass.instance_eval do
               def active_relation
-                raise "stubbed models are not allowed to access the database"
+                raise ERROR_MESSAGE
               end
             end
             klass
           end
-          
+
           def reload
-            raise "stubbed models are not allowed to access the database"
+            raise ERROR_MESSAGE
           end
         end
       end
