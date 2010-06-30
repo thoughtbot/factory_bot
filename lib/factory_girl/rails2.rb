8c4543166a8c406d93c27ea1df965602ba1b8f83
Elliot Crosby-McCullough and Chris Zetter <zetter@gmail.com>
2010-06-30 14:11:29 +0100

    Add support back in for subdirectory requires.
    Poor old rails 2.x was forgotten about in fc8ce3ab7.
    
    Closes #65

--- lib/factory_girl/rails2.rb
+++ lib/factory_girl/rails2.rb
@@ -1,1 +1,7 @@
-Rails.configuration.after_initialize { FactoryGirl.find_definitions }
+Rails.configuration.after_initialize do
+  FactoryGirl.definition_file_paths = [
+    File.join(Rails.root, 'test', 'factories'),
+    File.join(Rails.root, 'spec', 'factories')
+  ]
+  FactoryGirl.find_definitions
+end
