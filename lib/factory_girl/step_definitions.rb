thoughtbot/factory_bot

commit 1b28476713df98bed3963521e200ed34485a4209

Allow recursive associations from cucumber table arguments

lib/factory_girl/step_definitions.rb
@@ -1,11 +1,17 @@
 module FactoryGirlStepHelpers
+
   def convert_association_string_to_instance(factory_name, assignment)
     attribute, value = assignment.split(':', 2)
     return if value.blank?
-    attributes = convert_human_hash_to_attribute_hash(attribute => value.strip)
     factory = FactoryGirl.factory_by_name(factory_name)
+    attributes = convert_human_hash_to_attribute_hash({attribute => value.strip}, factory.associations)
+    attributes_find = {}
+    attributes.each do |k, v|
+      k = "#{k}_id" if v.is_a? ActiveRecord::Base
+      attributes_find[k] = v
+    end
     model_class = factory.build_class
-    model_class.find(:first, :conditions => attributes) or
+    model_class.find(:first, :conditions => attributes_find) or
       Factory(factory_name, attributes)
   end
 
@@ -51,3 +57,4 @@ FactoryGirl.factories.values.each do |factory|
     end
   end
 end
+

features/factory_girl_steps.feature
@@ -123,3 +123,29 @@ Feature: Use step definitions generated by factories
     And 2 categories exist
     And 2 categories exist with a name of "Future"
     Then there should be 6 categories
+
+  Scenario: create a post with an existing category group
+    Given the following category exists:
+      | ID  | name    | category group |
+      | 123 | fiction | Name: books    |
+    And the following post exists:
+      | Title                      | Author    | Category                    |
+      | a title                    | Name: Joe | Category Group: Name: books |
+    Then I should find the following for the last post:
+      | title   | category_id |
+      | a title | 123         |
+
+  Scenario: create a post with an existing category group and a new category
+    Given the following category group exists:
+      | ID  | name  |
+      | 456 | books |
+    And the following post exists:
+      | Title     | Author    | Category                    |
+      | a title   | Name: Joe | Category Group: Name: books |
+    Then I should find the following for the last post:
+      | title   |
+      | a title |
+    And I should find the following for the last category:
+      | category_group_id |
+      | 456               |
+

features/step_definitions/database_steps.rb
@@ -15,4 +15,6 @@ Before do
   Post.delete_all
   User.delete_all
   Category.delete_all
+  CategoryGroup.delete_all
 end
+

features/support/factories.rb
@@ -12,7 +12,12 @@ class CreateSchema < ActiveRecord::Migration
       t.string  :body
     end
 
+    create_table :category_groups, :force => true do |t|
+      t.string :name
+    end
+
     create_table :categories, :force => true do |t|
+      t.integer :category_group_id
       t.string :name
     end
 
@@ -28,7 +33,11 @@ CreateSchema.suppress_messages { CreateSchema.migrate(:up) }
 class User < ActiveRecord::Base
 end
 
+class CategoryGroup < ActiveRecord::Base
+end
+
 class Category < ActiveRecord::Base
+  belongs_to :category_group
 end
 
 class Post < ActiveRecord::Base
@@ -48,6 +57,11 @@ end
 
 Factory.define :category do |f|
   f.name "programming"
+  f.association :category_group
+end
+
+Factory.define :category_group do |f|
+  f.name "tecnhology"
 end
 
 Factory.define :post do |f|
@@ -60,3 +74,4 @@ Factory.define :non_active_record do |f|
 end
 
 require 'factory_girl/step_definitions'
+

commit 2f41dc729e4f2f7a39da426ea951c37dbda551d2

Break out step_definitions into strategies for finding and creating attributes
The change 1b28476713df98bed3963521e200ed34485a4209 introduced an issue
where projects not using ActiveRecord would fail because the constant
wasn't defined.

This removes the reliance on ActiveRecord and uses different strategies
for building hashes from cucumber tables based on whether the attributes
are finding a record in the database or creating an instance with
factory girl.

lib/factory_girl/step_definitions.rb
@@ -1,24 +1,92 @@
 module FactoryGirlStepHelpers
-
-  def convert_association_string_to_instance(factory_name, assignment)
-    attribute, value = assignment.split(':', 2)
-    return if value.blank?
-    factory = FactoryGirl.factory_by_name(factory_name)
-    attributes = convert_human_hash_to_attribute_hash({attribute => value.strip}, factory.associations)
-    model_class = factory.build_class
-    model_class.find(:first, :conditions => attributes) or
-      FactoryGirl.create(factory_name, attributes)
+  def convert_human_hash_to_attribute_hash(human_hash, associations = [])
+    HumanHashToAttributeHash.new(human_hash, associations).attributes
   end
 
-  def convert_human_hash_to_attribute_hash(human_hash, associations = [])
-    human_hash.inject({}) do |attribute_hash, (human_key, value)|
-      key = human_key.downcase.gsub(' ', '_').to_sym
-      if association = associations.detect {|association| association.name == key }
-        association_instance = convert_association_string_to_instance(association.factory, value)
-        key = "#{key}_id"
-        value = association_instance.id if association_instance
+  class HumanHashToAttributeHash
+    attr_reader :associations
+
+    def initialize(human_hash, associations)
+      @human_hash   = human_hash
+      @associations = associations
+    end
+
+    def attributes(strategy = CreateAttributes)
+      @human_hash.inject({}) do |attribute_hash, (human_key, value)|
+        attributes = strategy.new(self, *process_key_value(human_key, value))
+        attribute_hash.merge({ attributes.key => attributes.value })
       end
-      attribute_hash.merge(key => value)
+    end
+
+    private
+
+    def process_key_value(key, value)
+      [key.downcase.gsub(' ', '_').to_sym, value.strip]
+    end
+
+    class AssociationManager
+      def initialize(human_hash_to_attributes_hash, key, value)
+        @human_hash_to_attributes_hash = human_hash_to_attributes_hash
+        @key   = key
+        @value = value
+      end
+
+      def association
+        @human_hash_to_attributes_hash.associations.detect {|association| association.name == @key }
+      end
+
+      def association_instance
+        return unless association
+
+        if attributes_hash = nested_attribute_hash
+          factory.build_class.find(:first, :conditions => attributes_hash.attributes(FindAttributes)) or
+          FactoryGirl.create(association.factory, attributes_hash.attributes)
+        end
+      end
+
+      private
+
+      def factory
+        FactoryGirl.factory_by_name(association.factory)
+      end
+
+      def nested_attribute_hash
+        attribute, value = @value.split(':', 2)
+        return if value.blank?
+
+        HumanHashToAttributeHash.new({ attribute => value }, factory.associations)
+      end
+    end
+
+    class AttributeStrategy
+      attr_reader :key, :value, :association_manager
+
+      def initialize(human_hash_to_attributes_hash, key, value)
+        @association_manager = AssociationManager.new(human_hash_to_attributes_hash, key, value)
+        @key   = key
+        @value = value
+      end
+    end
+
+    class FindAttributes < AttributeStrategy
+      def initialize(human_hash_to_attributes_hash, key, value)
+        super
+
+        if association_manager.association
+          @key = "#{@key}_id"
+          @value = association_manager.association_instance.try(:id)
+        end
+      end
+    end
+
+    class CreateAttributes < AttributeStrategy
+      def initialize(human_hash_to_attributes_hash, key, value)
+        super
+
+        if association_manager.association
+          @value = association_manager.association_instance
+        end
+      end
     end
   end
 end