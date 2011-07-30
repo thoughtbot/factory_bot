841e012c5dffa415571f85f11ca53197d49dd695
Joshua Clayton <jclayton@thoughtbot.com>
2011-07-30 09:49:12 -0400

    Add acceptance spec covering assigning associations via :factory
    Closes #100

--- spec/acceptance/attribute_aliases_spec.rb
+++ spec/acceptance/attribute_aliases_spec.rb
@@ -3,23 +3,44 @@ require 'acceptance/acceptance_helper'
 
 describe "attribute aliases" do
   before do
-    define_model('User')
+    define_model('User', :name => :string, :age => :integer)
 
     define_model('Post', :user_id => :integer) do
       belongs_to :user
     end
 
     FactoryGirl.define do
-      factory :user
+      factory :user do
+        factory :user_with_name do
+          name "John Doe"
+        end
+      end
 
       factory :post do
         user
       end
+
+      factory :post_with_named_user, :class => Post do
+        user :factory => :user_with_name, :age => 20
+      end
     end
   end
 
-  it "doesn't assign both an association and its foreign key" do
-    FactoryGirl.build(:post, :user_id => 1).user_id.should == 1
+  context "assigning an association by foreign key" do
+    subject { FactoryGirl.build(:post, :user_id => 1) }
+
+    it "doesn't assign both an association and its foreign key" do
+      subject.user_id.should == 1
+    end
+  end
+
+  context "assigning an association by passing factory" do
+    subject { FactoryGirl.create(:post_with_named_user).user }
+
+    it "assigns attributes correctly" do
+      subject.name.should == "John Doe"
+      subject.age.should == 20
+    end
   end
 end
 
