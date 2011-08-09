c7e93789d14ff20db8dc4a91c019ec99b821e8cf
Thomas Walpole <twalpole@gmail.com>
2011-08-09 13:50:14 -0700

    more description names in spec

--- spec/acceptance/attributes_ordered_spec.rb
+++ spec/acceptance/attributes_ordered_spec.rb
@@ -5,19 +5,19 @@ describe "a generated attributes hash where order matters" do
   include FactoryGirl::Syntax::Methods
 
   before do
-    define_model('ParentModel', :a   => :integer,
-                              :b    => :integer,
-                              :c => :integer,
-                              :d => :integer)
+    define_model('ParentModel', :static           => :integer,
+                                :evaluates_first  => :integer,
+                                :evaluates_second => :integer,
+                                :evaluates_third  => :integer)
 
     FactoryGirl.define do
       factory :parent_model do
-        b { a }
-        c { b }
-        d { c }
+        evaluates_first  { static }
+        evaluates_second { evaluates_first }
+        evaluates_third  { evaluates_second }
         
         factory :child_model do
-          a 1
+          static 1
         end
       end
     end
@@ -26,9 +26,9 @@ describe "a generated attributes hash where order matters" do
   subject { FactoryGirl.build(:child_model) }
 
   it "assigns attributes in the order they're defined with preference to static attributes" do
-    subject[:b].should == 1
-    subject[:c].should == 1
-    subject[:d].should == 1
+    subject[:evaluates_first].should == 1
+    subject[:evaluates_second].should == 1
+    subject[:evaluates_third].should == 1
   end
 
 end
