be34e0c973ee620fc6389f5002fe1a5d19eac36a
Chris Griego <cgriego@gmail.com>
2011-08-27 16:39:52 -0500

    Load factories from /factories at the Rails root, just like the default definition_file_paths.

--- lib/factory_girl/rails2.rb
+++ lib/factory_girl/rails2.rb
@@ -1,5 +1,6 @@
 Rails.configuration.after_initialize do
   FactoryGirl.definition_file_paths = [
+    File.join(Rails.root, 'factories'),
     File.join(Rails.root, 'test', 'factories'),
     File.join(Rails.root, 'spec', 'factories')
   ]
