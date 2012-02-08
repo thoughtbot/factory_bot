88be1de040c7d5848ee5eff7f17f993951dfc59b
Joshua Clayton <jclayton@thoughtbot.com>
2012-02-08 14:17:04 -0500

    Remove comment since the behavior it describes has been moved

--- lib/factory_girl/strategy.rb
+++ lib/factory_girl/strategy.rb
@@ -9,38 +9,6 @@ module FactoryGirl
   class Strategy #:nodoc:
     include Observable
 
-    # Generates an association using the current build strategy.
-    #
-    # Arguments:
-    #   name: (Symbol)
-    #     The name of the factory that should be used to generate this
-    #     association.
-    #   attributes: (Hash)
-    #     A hash of attributes that should be overridden for this association.
-    #
-    # Returns:
-    #   The generated association for the current build strategy. Note that
-    #   associations are not generated for the attributes_for strategy. Returns
-    #   nil in this case.
-    #
-    # Example:
-    #
-    #   factory :user do
-    #     # ...
-    #   end
-    #
-    #   factory :post do
-    #     # ...
-    #     author { |post| post.association(:user, :name => 'Joe') }
-    #   end
-    #
-    #   # Builds (but doesn't save) a Post and a User
-    #   FactoryGirl.build(:post)
-    #
-    #   # Builds and saves a User, builds a Post, assigns the User to the
-    #   # author association, and saves the Post.
-    #   FactoryGirl.create(:post)
-    #
     def association(runner, overrides)
       raise NotImplementedError, "Strategies must return an association"
     end
