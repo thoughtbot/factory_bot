ff3e7c28c8e28c180c7964685de017aaa2ccba3b
Eugene Diachkin <ineu@starlightsys.com>
2012-05-22 22:49:15 +0300

    Allow to use AR class names as arguments to create, build etc

--- lib/factory_girl.rb
+++ lib/factory_girl.rb
@@ -65,6 +65,7 @@ module FactoryGirl
   end
 
   def self.factory_by_name(name)
+    name = name.to_s.underscore unless name.class.in? String, Symbol # looks like AR object
     factories.find(name)
   end
 
--- spec/factory_girl_spec.rb
+++ spec/factory_girl_spec.rb
@@ -10,6 +10,13 @@ describe FactoryGirl do
     FactoryGirl.factory_by_name(factory.name).should == factory
   end
 
+  it "finds registered factory by object name" do
+    class MultiWordClass < ActiveRecord::Base; end
+    fact = FactoryGirl::Factory.new :multi_word_class
+    FactoryGirl.register_factory(fact)
+    FactoryGirl.factory_by_name(MultiWordClass).should == fact
+  end
+
   it "finds a registered sequence" do
     FactoryGirl.register_sequence(sequence)
     FactoryGirl.sequence_by_name(sequence.name).should == sequence
