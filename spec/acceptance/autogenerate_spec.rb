9c40f312574e71007fe19a807c3c26e4a686f95b
Hugo Roque <hugolnx@gmail.com>
2012-09-30 00:53:35 -0300

    cleaning test of autogenerate

--- spec/acceptance/autogenerate_spec.rb
+++ spec/acceptance/autogenerate_spec.rb
@@ -5,10 +5,6 @@ describe "autogenerate" do
 
   context 'passing string with #N' do
     it "generates string increasing #N sequentialy" do
-      FactoryGirl.define do
-        sequence(:size, "a", aliases: [:count, :length]) {|n| "called-#{n}" }
-      end
-
       autogenerate("nickname#N@email.com").should == "nickname1@email.com"
       autogenerate("nickname#N@email.com").should == "nickname2@email.com"
       autogenerate("nickname#N@email.com").should == "nickname3@email.com"
