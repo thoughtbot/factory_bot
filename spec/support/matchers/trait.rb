938b17eb168a25072bb49cf876ec72532b06626a
Potapov Sergey <blake131313@gmail.com>
2013-12-12 14:44:44 +0200

    Use Set#any? method in trait matcher

--- spec/support/matchers/trait.rb
+++ spec/support/matchers/trait.rb
@@ -1,9 +1,8 @@
 RSpec::Matchers.define :have_trait do |trait_name|
   match do |instance|
-    traits = instance.defined_traits.select do |trait|
+    instance.defined_traits.any? do |trait|
       trait.name == trait_name && trait.send(:block) == @block
     end
-    traits.any?
   end
 
   chain :with_block do |block|
