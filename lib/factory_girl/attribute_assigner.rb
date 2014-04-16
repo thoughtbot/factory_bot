413684ea045395ed5eb3afc67deb90e48e46dbf6
Peter Jaros <peter.a.jaros@gmail.com>
2014-04-16 16:17:51 -0400

    FactoryGirl refuses to assign to a private setter.

--- lib/factory_girl/attribute_assigner.rb
+++ lib/factory_girl/attribute_assigner.rb
@@ -13,7 +13,7 @@ module FactoryGirl
       @evaluator.instance = build_class_instance
       build_class_instance.tap do |instance|
         attributes_to_set_on_instance.each do |attribute|
-          instance.send("#{attribute}=", get(attribute))
+          instance.public_send("#{attribute}=", get(attribute))
           @attribute_names_assigned << attribute
         end
       end
