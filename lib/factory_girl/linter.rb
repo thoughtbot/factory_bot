ed111289feeb6986d369d8cae3d0a3a252f66006
Joshua Clayton <jclayton@thoughtbot.com>
2014-10-13 17:24:17 -0400

    WIP: use facade to manage invalid factories and their errors

--- lib/factory_girl/linter.rb
+++ lib/factory_girl/linter.rb
@@ -5,7 +5,10 @@ module FactoryGirl
     end
 
     def initialize(factories_to_lint)
-      @factories_to_lint = factories_to_lint
+      @factories_to_lint = factories_to_lint.map do |factory|
+        ErroringModel.new(factory)
+      end
+
       @invalid_factories = calculate_invalid_factories
     end
 
@@ -20,21 +23,51 @@ module FactoryGirl
     attr_reader :factories_to_lint, :invalid_factories
 
     def calculate_invalid_factories
-      factories_to_lint.select do |factory|
-        built_factory = FactoryGirl.build(factory.name)
-
-        if built_factory.respond_to?(:valid?)
-          !built_factory.valid?
-        end
-      end
+      factories_to_lint.reject(&:valid?)
     end
 
     def error_message
       <<-ERROR_MESSAGE.strip
 The following factories are invalid:
 
-#{invalid_factories.map {|factory| "* #{factory.name}" }.join("\n")}
+#{invalid_factories.map {|factory| "* #{factory.name}: #{factory.errors}" }.join("\n")}
       ERROR_MESSAGE
     end
+
+    class ErroringModel
+      def initialize(factory)
+        @factory = factory
+        @built_factory = FactoryGirl.build(factory.name)
+      end
+
+      def valid?
+        if built_factory.respond_to?(:valid?)
+          built_factory.valid?
+        else
+          true
+        end
+      end
+
+      def errors
+        error_list.to_sentence
+      end
+
+      def name
+        factory.name
+      end
+
+      protected
+
+      attr_reader :factory, :built_factory
+
+      def error_list
+        if built_factory.respond_to?(:errors) && built_factory.errors.respond_to?(:full_messages)
+          built_factory.errors.full_messages
+        else
+          []
+        end
+      end
+    end
+
   end
 end
