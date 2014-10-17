f451e4a4193d7c2864b43e7cfa4d7ce699a65820
Joshua Clayton <jclayton@thoughtbot.com>
2014-10-17 12:03:22 -0400

    Fresh pass at displaying more detail during linting

--- lib/factory_girl/linter.rb
+++ lib/factory_girl/linter.rb
@@ -20,20 +20,26 @@ module FactoryGirl
     attr_reader :factories_to_lint, :invalid_factories
 
     def calculate_invalid_factories
-      factories_to_lint.select do |factory|
-        built_factory = FactoryGirl.build(factory.name)
-
-        if built_factory.respond_to?(:valid?)
-          !built_factory.valid?
+      factories_to_lint.inject({}) do |result, factory|
+        begin
+          FactoryGirl.create(factory.name)
+        rescue => error
+          result[factory] = error
         end
+
+        result
       end
     end
 
     def error_message
+      lines = invalid_factories.map do |factory, exception|
+        "* #{factory.name} - #{exception.message} (#{exception.class.name})"
+      end
+
       <<-ERROR_MESSAGE.strip
 The following factories are invalid:
 
-#{invalid_factories.map {|factory| "* #{factory.name}" }.join("\n")}
+#{lines.join("\n")}
       ERROR_MESSAGE
     end
   end
