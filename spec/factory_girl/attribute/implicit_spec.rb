0f48b00722a81991527efd341d2ce3ec1d50abff
Dusan Orlovic <duleorlovic@gmail.com>
2016-12-05 14:12:23 +0100

    Indend for second argument

--- spec/factory_girl/attribute/implicit_spec.rb
+++ spec/factory_girl/attribute/implicit_spec.rb
@@ -6,9 +6,8 @@ describe FactoryGirl::Declaration::Implicit do
     let(:first) { FactoryGirl::Declaration::Implicit.new(name) }
     subject { first == :some_symbol }
     it "raise error" do
-      expect { subject }.to raise_error(
-        NoMethodError,
-        /symbol for factory name/)
+      expect { subject }.to raise_error(NoMethodError,
+                                        /symbol for factory name/)
     end
   end
 end
