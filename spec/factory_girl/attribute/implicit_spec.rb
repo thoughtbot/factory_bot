be1beb2da68db98c4866e7115946447ad5df9d83
Dusan Orlovic <duleorlovic@gmail.com>
2016-12-05 14:09:34 +0100

    Proper indent

--- spec/factory_girl/attribute/implicit_spec.rb
+++ spec/factory_girl/attribute/implicit_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe FactoryGirl::Declaration::Implicit do
   context "using variable instead of symbol name" do
-  let(:name) { :first_name }
-  let(:first) { FactoryGirl::Declaration::Implicit.new(name) }
-  subject { first == :some_symbol }
+    let(:name) { :first_name }
+    let(:first) { FactoryGirl::Declaration::Implicit.new(name) }
+    subject { first == :some_symbol }
     it "raise error" do
-      expect { subject }.to raise_error(NoMethodError, /symbol for factory name/)
+      expect { subject }.to raise_error(
+        NoMethodError,
+        /symbol for factory name/)
     end
   end
 end
