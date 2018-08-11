40eed88bb9222ee09001fbc7d301527836e0d085
Daniel Colson <danieljamescolson@gmail.com>
2018-08-11 16:24:44 -0400

    Remove unused lib/factory_girl.rb
    Cleaning up after the rename

--- lib/factory_girl.rb
+++
@@ -1,5 +0,0 @@
-require "factory_bot"
-
-FactoryGirl = FactoryBot
-
-warn "The factory_girl gem has been deprecated and has been replaced by factory_bot. Please switch to factory_bot as soon as possible."
