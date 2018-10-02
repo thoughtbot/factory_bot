a9848fbf40224dcec13a4e3114c1550c1f976eae
Hunter Braun <hunter.braun@gmail.com>
2018-10-01 22:17:35 -0500

    Fix Style/EmptyMethod offenses

--- lib/factory_bot/strategy/null.rb
+++ lib/factory_bot/strategy/null.rb
@@ -1,11 +1,9 @@
 module FactoryBot
   module Strategy
     class Null
-      def association(runner)
-      end
+      def association(runner); end
 
-      def result(evaluation)
-      end
+      def result(evaluation); end
     end
   end
 end
