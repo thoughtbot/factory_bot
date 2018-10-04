e666b92336183105fa45366b46691946f0b91c13
Hunter Braun <hunter.braun@gmail.com>
2018-10-03 21:07:26 -0500

    Revert "Fix Performance/RegexpMatch offense"
    This reverts commit bb3392b3f98678771601348725873d2835be3d7e.

--- lib/factory_bot/aliases.rb
+++ lib/factory_bot/aliases.rb
@@ -10,7 +10,7 @@ module FactoryBot
 
   def self.aliases_for(attribute)
     aliases.map do |(pattern, replace)|
-      if pattern.match?(attribute.to_s)
+      if pattern.match(attribute.to_s)
         attribute.to_s.sub(pattern, replace).to_sym
       end
     end.compact << attribute
