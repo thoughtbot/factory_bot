7925c47653b4e3c06661ffefac192efcf0c48144
Daniel Colson <danieljamescolson@gmail.com>
2018-10-21 11:39:48 -0400

    Remove remaining RuboCop TODOs
    Closes #1202
    
    We have fixed the bulk of the RuboCop TODOs. The main TODO remaining
    (line length) involves a large number of files,
    and I don't think it is worth trying to update them all at once. I did
    fix a few of the worst offenders so we could bring the max down a bit.
    We can gradually bring this number down as we fix more of the
    violations.

--- spec/acceptance/nested_attributes_spec.rb
+++ spec/acceptance/nested_attributes_spec.rb
@@ -25,6 +25,8 @@ describe "association assignment from nested attributes" do
   end
 
   it "assigns the correct amount of comments when overridden" do
-    expect(FactoryBot.create(:post, comments_attributes: [FactoryBot.attributes_for(:comment)]).comments.count).to eq 1
+    post = FactoryBot.create(:post, comments_attributes: [FactoryBot.attributes_for(:comment)])
+
+    expect(post.comments.count).to eq 1
   end
 end
