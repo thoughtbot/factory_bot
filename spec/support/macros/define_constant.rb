05bf5a3713ffacffa09b78d67a4900c54a626b7e
Oliver Peate <oliverp@gmail.com>
2019-07-31 17:03:05 +0100

    Use sqlite in-memory DB in specs
    https://www.sqlite.org/inmemorydb.html

--- spec/support/macros/define_constant.rb
+++ spec/support/macros/define_constant.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   config.before(:all) do
     ActiveRecord::Base.establish_connection(
       adapter:  "sqlite3",
-      database: File.join(File.dirname(__FILE__), "test.db"),
+      database: ":memory:",
     )
   end
 
