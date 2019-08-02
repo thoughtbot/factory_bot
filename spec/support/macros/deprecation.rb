ecb56565a162d106dbc9722270b6f346ef3b69a7
Oliver Peate <oliverp@gmail.com>
2019-08-02 15:21:07 +0100

    Use temporary assignment helper to silence deprecations in specs

--- spec/support/macros/deprecation.rb
+++ spec/support/macros/deprecation.rb
@@ -1,18 +1,9 @@
 require "active_support"
 
-module SilenceDeprecation
-  def silence_deprecation(example)
-    cached_silenced = FactoryBot::Deprecation.silenced
-    FactoryBot::Deprecation.silenced = true
-    example.run
-    FactoryBot::Deprecation.silenced = cached_silenced
-  end
-end
-
 RSpec.configure do |config|
-  config.include SilenceDeprecation
-
   config.around :example, silence_deprecation: true do |example|
-    silence_deprecation(example)
+    with_temporary_assignment(FactoryBot::Deprecation, :silenced, true) do
+      example.run
+    end
   end
 end
