2a38afd16d4e5271c7e8a36e756fe85e2510d88b
Richie Thomas <richie@thoughtbot.com>
2019-10-11 11:05:15 -0700

    Merged a few tests with identical setup steps

--- spec/factory_bot/aliases_spec.rb
+++ spec/factory_bot/aliases_spec.rb
@@ -6,19 +6,14 @@ describe FactoryBot, "aliases" do
     expect(aliases).to include(:test_id)
   end
 
-  it "aliases for a foreign key should include the un-suffixed attribute name" do
+  it "aliases for a foreign key should include both the suffixed and un-suffixed variants" do
     aliases = FactoryBot.aliases_for(:test_id)
 
     expect(aliases).to include(:test)
-  end
-
-  it "aliases for a foreign key should include the original attribute name" do
-    aliases = FactoryBot.aliases_for(:test_id)
-
     expect(aliases).to include(:test_id)
   end
 
-  it "aliases for an attribute starting with an underscore should not include a non-underscored version" do
+  it "aliases for an attribute which starts with an underscore should not include a non-underscored version" do
     aliases = FactoryBot.aliases_for(:_id)
 
     expect(aliases).not_to include(:id)
@@ -26,17 +21,11 @@ describe FactoryBot, "aliases" do
 end
 
 describe FactoryBot, "after defining an alias" do
-  it "the list of aliases should include an un-suffixed equivalent" do
+  it "the list of aliases should include a variant with no suffix at all, and one with an '_id' suffix" do
     FactoryBot.aliases << [/(.*)_suffix/, '\1']
     aliases = FactoryBot.aliases_for(:test_suffix)
 
     expect(aliases).to include(:test)
-  end
-
-  it "the list of aliases should include the original name plus an '_id' suffix" do
-    FactoryBot.aliases << [/(.*)_suffix/, '\1']
-    aliases = FactoryBot.aliases_for(:test_suffix)
-
     expect(aliases).to include(:test_suffix_id)
   end
 end
