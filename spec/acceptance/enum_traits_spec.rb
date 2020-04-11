97107915669e0e266339b183d696a1f5b90f8db7
Frida Casas <fridacasas.fc@gmail.com>
2020-04-11 12:07:57 -0400

    Refactor enum traits specs to two main contexts:
    - automatically_define_enum_traits = false
    - automatically_define_enum_traits = true
    
    When setting `FactoryBot.automatically_define_enum_traits = false`, we
    allow for 'turning off' automatically built traits from enum. Add test
    to confirm that attempting to build automatically while this is false
    raises an error.

--- spec/acceptance/enum_traits_spec.rb
+++ spec/acceptance/enum_traits_spec.rb
@@ -1,14 +1,12 @@
 describe "enum traits" do
-  it "builds traits for each enumerated value" do
-    with_temporary_assignment(FactoryBot, :automatically_define_enum_traits, false) do
+  context "when automatically_define_enum_traits is true" do
+    it "builds traits automatically for model enum field" do
       define_model("Task", status: :integer) do
         enum status: { queued: 0, started: 1, finished: 2 }
       end
 
       FactoryBot.define do
-        factory :task do
-          traits_for_enum(:status)
-        end
+        factory :task
       end
 
       Task.statuses.each_key do |trait_name|
@@ -19,119 +17,145 @@ describe "enum traits" do
 
       Task.reset_column_information
     end
-  end
 
-  it "builds traits automatically for model enum field" do
-    define_model("Task", status: :integer) do
-      enum status: { queued: 0, started: 1, finished: 2 }
-    end
+    it "prefers user defined traits over automatically built traits" do
+      define_model("Task", status: :integer) do
+        enum status: { queued: 0, started: 1, finished: 2 }
+      end
 
-    FactoryBot.define do
-      factory :task
-    end
+      FactoryBot.define do
+        factory :task do
+          trait :queued do
+            status { :finished }
+          end
 
-    Task.statuses.each_key do |trait_name|
-      task = FactoryBot.build(:task, trait_name)
+          trait :started do
+            status { :finished }
+          end
 
-      expect(task.status).to eq(trait_name)
-    end
-
-    Task.reset_column_information
-  end
-
-  it "prefers user defined traits over automatically built traits" do
-    define_model("Task", status: :integer) do
-      enum status: { queued: 0, started: 1, finished: 2 }
-    end
-
-    FactoryBot.define do
-      factory :task do
-        trait :queued do
-          status { :finished }
-        end
-
-        trait :started do
-          status { :finished }
-        end
-
-        trait :finished do
-          status { :finished }
+          trait :finished do
+            status { :finished }
+          end
         end
       end
+
+      Task.statuses.each_key do |trait_name|
+        task = FactoryBot.build(:task, trait_name)
+
+        expect(task.status).to eq("finished")
+      end
+
+      Task.reset_column_information
     end
 
-    Task.statuses.each_key do |trait_name|
-      task = FactoryBot.build(:task, trait_name)
+    it "builds traits for each enumerated value using a provided list of values as a Hash" do
+      statuses = { queued: 0, started: 1, finished: 2 }
 
-      expect(task.status).to eq("finished")
+      define_model "Task", status: :integer
+
+      FactoryBot.define do
+        factory :task do
+          traits_for_enum :status, statuses
+        end
+      end
+
+      statuses.each do |trait_name, trait_value|
+        task = FactoryBot.build(:task, trait_name)
+
+        expect(task.status).to eq(trait_value)
+      end
+
+      Task.reset_column_information
     end
 
-    Task.reset_column_information
+    it "builds traits for each enumerated value using a provided list of values as an Array" do
+      statuses = %w[queued started finished]
+
+      define_model "Task", status: :string
+
+      FactoryBot.define do
+        factory :task do
+          traits_for_enum :status, statuses
+        end
+      end
+
+      statuses.each do |trait_name|
+        task = FactoryBot.build(:task, trait_name)
+
+        expect(task.status).to eq(trait_name)
+      end
+
+      Task.reset_column_information
+    end
+
+    it "builds traits for each enumerated value using a custom enumerable" do
+      statuses = define_class("Statuses") do
+        include Enumerable
+
+        def each(&block)
+          ["queued", "started", "finished"].each(&block)
+        end
+      end.new
+
+      define_model "Task", status: :string
+
+      FactoryBot.define do
+        factory :task do
+          traits_for_enum :status, statuses
+        end
+      end
+
+      statuses.each do |trait_name|
+        task = FactoryBot.build(:task, trait_name)
+
+        expect(task.status).to eq(trait_name)
+      end
+
+      Task.reset_column_information
+    end
   end
 
-  it "builds traits for each enumerated value using a provided list of values as a Hash" do
-    statuses = { queued: 0, started: 1, finished: 2 }
+  context "when automatically_define_enum_traits is false" do
+    it "raises error when attempting to build traits automatically" do
+      with_temporary_assignment(FactoryBot, :automatically_define_enum_traits, false) do
+        define_model("Task", status: :integer) do
+          enum status: { queued: 0, started: 1, finished: 2 }
+        end
 
-    define_model "Task", status: :integer
+        FactoryBot.define do
+          factory :task
+        end
 
-    FactoryBot.define do
-      factory :task do
-        traits_for_enum :status, statuses
+        Task.statuses.each_key do |trait_name|
+          expect { FactoryBot.build(:task, trait_name) }.to raise_error(
+            KeyError, "Trait not registered: \"#{trait_name}\""
+          )
+        end
+
+        Task.reset_column_information
       end
     end
 
-    statuses.each do |trait_name, trait_value|
-      task = FactoryBot.build(:task, trait_name)
+    it "builds traits for each enumerated value when traits_for_enum are specified" do
+      with_temporary_assignment(FactoryBot, :automatically_define_enum_traits, false) do
+        define_model("Task", status: :integer) do
+          enum status: { queued: 0, started: 1, finished: 2 }
+        end
 
-      expect(task.status).to eq(trait_value)
-    end
+        FactoryBot.define do
+          factory :task do
+            traits_for_enum(:status)
+          end
+        end
 
-    Task.reset_column_information
-  end
+        Task.statuses.each_key do |trait_name|
+          task = FactoryBot.build(:task, trait_name)
 
-  it "builds traits for each enumerated value using a provided list of values as an Array" do
-    statuses = %w[queued started finished]
+          expect(task.status).to eq(trait_name)
+        end
 
-    define_model "Task", status: :string
-
-    FactoryBot.define do
-      factory :task do
-        traits_for_enum :status, statuses
+        Task.reset_column_information
       end
     end
-
-    statuses.each do |trait_name|
-      task = FactoryBot.build(:task, trait_name)
-
-      expect(task.status).to eq(trait_name)
-    end
-
-    Task.reset_column_information
-  end
-
-  it "builds traits fir each enumerated value using a custom enumerable" do
-    statuses = define_class("Statuses") do
-      include Enumerable
-
-      def each(&block)
-        ["queued", "started", "finished"].each(&block)
-      end
-    end.new
-
-    define_model "Task", status: :string
-
-    FactoryBot.define do
-      factory :task do
-        traits_for_enum :status, statuses
-      end
-    end
-
-    statuses.each do |trait_name|
-      task = FactoryBot.build(:task, trait_name)
-
-      expect(task.status).to eq(trait_name)
-    end
-
-    Task.reset_column_information
   end
 end
