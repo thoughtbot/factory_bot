8bbfca2be84493c85aa6e88bfc8038b19fafa51a
Daniel Colson <danieljamescolson@gmail.com>
2020-04-23 19:15:23 -0400

    Remove unnecessary cop suppression
    The line length was fine here, and plus it was in the wrong namespace
    for recent versions of rubocop

--- lib/factory_bot/definition_proxy.rb
+++ lib/factory_bot/definition_proxy.rb
@@ -88,7 +88,7 @@ module FactoryBot
     #   end
     #
     # are equivalent.
-    def method_missing(name, *args, &block) # rubocop:disable Style/MissingRespondToMissing, Style/MethodMissingSuper, Metrics/LineLength
+    def method_missing(name, *args, &block) # rubocop:disable Style/MissingRespondToMissing, Style/MethodMissingSuper
       association_options = args.first
 
       if association_options.nil?
