9ea565f8daf90834ac2a80410e2ef196ce2b44e5
Daniel Colson <danieljamescolson@gmail.com>
2020-06-21 21:58:44 -0400

    Raise more helpful error for invalid attribute
    Closes #1391
    
    Along the same lines as #1286, this commit raises a more helpful error
    if somebody passes an implicit declaration as an association argument.
    
    Before this commit, an association with an implicit trait passed as an
    override attribute:
    
    ```rb
    person factory: :user, invalid_attribute: implicit_trait
    ```
    
    Would raise an error `KeyError: Trait not registered: "implicit_trait"`.
    
    This is potentially confusing, since the author probably didn't intend
    to define an implicit trait.
    
    After this commit, this will raise a more helpful error:
    
    ```
    ArgumentError: Association 'person' received an invalid attribute override.
    Did you mean? 'invalid_attribute}: :implicit_trait}'
    ```

--- spec/acceptance/associations_spec.rb
+++ spec/acceptance/associations_spec.rb
@@ -1,6 +1,6 @@
 describe "associations" do
   context "when accidentally using an implicit delcaration for the factory" do
-    it "raises an error about the trait not being registered" do
+    it "raises an error" do
       define_class("Post")
 
       FactoryBot.define do
@@ -13,6 +13,24 @@ describe "associations" do
         ArgumentError,
         "Association 'author' received an invalid factory argument.\n" \
         "Did you mean? 'factory: :user'\n"
+      )
+    end
+  end
+
+  context "when accidentally using an implicit delcaration as an override" do
+    it "raises an error" do
+      define_class("Post")
+
+      FactoryBot.define do
+        factory :post do
+          author factory: :user, invalid_attribute: implicit_trait
+        end
+      end
+
+      expect { FactoryBot.build(:post) }.to raise_error(
+        ArgumentError,
+        "Association 'author' received an invalid attribute override.\n" \
+        "Did you mean? 'invalid_attribute: :implicit_trait'\n"
       )
     end
   end
