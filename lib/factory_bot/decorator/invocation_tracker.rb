e3ee350e2f5255cc43532b3faebece61d22c1e1f
Adam Hess <HParker@github.com>
2020-06-23 09:54:55 -0700

    Fix ruby 2.7 kwargs warning
    Ruby 2.7 deprecated passing kwargs when the method expects a hash or passing a hash when the method expects kwargs. In factory_bot, this creates the warning:
    
    ```
    /Users/hparker/code/factory_bot/lib/factory_bot/decorator/new_constructor.rb:9: warning: Using the last argument as keyword parameters is deprecated; maybe ** should be added to the call
    /Users/hparker/code/factory_bot/spec/acceptance/initialize_with_spec.rb:220: warning: The called method `initialize' is defined here
    ```
    
    We can fix this warning by updating the syntax. We need to include `**kwargs` in the `method_missing` calls when we are on ruby 2.7 or later.
    In decorator.rb, adding `**kwargs` alone doesn't work since adding `**kwargs` can change what arguments remain in the `args`.
    
    In this case we have to class eval the method if we are running ruby 2.7. This way the syntax is valid in previous versions and we can use the `...` operator which allows us to avoid changing the arguments passed on in method missing.
    
    Co-authored-by: Lee Quarella <leequarella@gmail.com>

--- lib/factory_bot/decorator/invocation_tracker.rb
+++ lib/factory_bot/decorator/invocation_tracker.rb
@@ -6,9 +6,16 @@ module FactoryBot
         @invoked_methods = []
       end
 
-      def method_missing(name, *args, &block) # rubocop:disable Style/MissingRespondToMissing
-        @invoked_methods << name
-        super
+      if ::Gem::Version.new(::RUBY_VERSION) >= ::Gem::Version.new("2.7")
+        def method_missing(name, *args, **kwargs, &block) # rubocop:disable Style/MissingRespondToMissing
+          @invoked_methods << name
+          super
+        end
+      else
+        def method_missing(name, *args, &block) # rubocop:disable Style/MissingRespondToMissing
+          @invoked_methods << name
+          super
+        end
       end
 
       def __invoked_methods__
