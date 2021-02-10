9683f3bf2e236446ae054e4eceae2158f3567cfb
Matt Larraz <mlarraz@users.noreply.github.com>
2021-02-10 14:37:41 -0500

    Skip did_you_mean specs on Ruby < 2.7

--- spec/acceptance/traits_spec.rb
+++ spec/acceptance/traits_spec.rb
@@ -324,12 +324,15 @@ describe "looking up traits that don't exist" do
         end
       end
 
+      msg = 'Trait not registered: "not_quite" referenced within "user" definition'
+
+      if RUBY_VERSION >= "2.7.0"
+        msg += "\n" + 'Did you mean?  "not_quit"'
+      end
+
       expect { FactoryBot.build(:user) }.to raise_error(
         KeyError,
-        <<~MSG.strip
-          Trait not registered: "not_quite" referenced within "user" definition
-          Did you mean?  "not_quit"
-        MSG
+        msg
       )
     end
   end
