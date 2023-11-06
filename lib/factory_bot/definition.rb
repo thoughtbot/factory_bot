f3e92652c8c7d460d06a7d6e1c67282171712761
Michael Smith <mike@spokefire.co.uk>
2023-11-06 15:19:33 +0000

    Add per model automatically_define_enum_traits option
    Currently you can only specify whether to automatically define enum traits at
    a global level, through `FactoryBot.automatically_define_enum_traits`.
    This means that an entire codebase has to either opt-in, or opt-out
    from automatically defining enum traits. If you are in a large,
    established codebase with lots of enum's, this is quite hard to change
    globally when you find that automatically defining them doesn't fit for
    your new use case.
    
    If we could override this at a per-factory level, we could allow
    individual factories to override the global setting where appropriate,
    in order to do customise them where necessary.
    
    Given `FactoryBot.automatically_define_enum_traits` being set to `true`,
    and a model called `Task` with the following enum definition:
    
    ```
    class Task
      enum :confirmed_by, [:user, :organisation], prefix: true
    end
    ```
    
    You would be able to override disable this on a per-factory basis like so:
    
    ```
    FactoryBot.define do
      factory :task, automatically_define_enum_traits: false do
        confirmed_by { :user }
    
        trait :org_confirmed do
          confirmed_by { :organisation }
        end
      end
    end
    ```
    
    If `FactoryBot.automatically_define_enum_traits` was instead set to
    `false`, then the same model with a factory override set to `true` you
    would end up with the following automatic traits:
    
    ```
    FactoryBot.define do
      factory :task, automatically_define_enum_traits: true do
        # The :user and :organisation traits below would be automatically
        # defined in the following way:
        #
        # trait :user do
        #   confirmed_by { :user }
        # end
    
        # trait :organisation do
        #   confirmed_by { :organisation }
        # end
      end
    end
    ```
    
    Fixes: https://github.com/thoughtbot/factory_bot/issues/1597
    
    Co-Authored-By: Julia Chan <julia.chan@freeagent.com>

--- lib/factory_bot/definition.rb
+++ lib/factory_bot/definition.rb
@@ -3,7 +3,7 @@ module FactoryBot
   class Definition
     attr_reader :defined_traits, :declarations, :name, :registered_enums
 
-    def initialize(name, base_traits = [])
+    def initialize(name, base_traits = [], automatically_define_enum_traits = nil)
       @name = name
       @declarations = DeclarationList.new(name)
       @callbacks = []
@@ -15,6 +15,7 @@ module FactoryBot
       @constructor = nil
       @attributes = nil
       @compiled = false
+      @automatically_define_enum_traits = automatically_define_enum_traits
       @expanded_enum_traits = false
     end
 
@@ -195,8 +196,13 @@ module FactoryBot
     end
 
     def automatically_register_defined_enums?(klass)
-      FactoryBot.automatically_define_enum_traits &&
-        klass.respond_to?(:defined_enums)
+      automatically_define_enum_traits = if @automatically_define_enum_traits.nil?
+        FactoryBot.automatically_define_enum_traits
+      else
+        @automatically_define_enum_traits
+      end
+
+      automatically_define_enum_traits && klass.respond_to?(:defined_enums)
     end
   end
 end
