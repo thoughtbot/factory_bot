2073d065d70a89b8f99285d41d35a50bd59e0fb7
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-08-13 11:06:12 +0200

    Fix Rails 7.2 compatibility
    In order to use parts of active_support, active_support itself must be required first.
    https://guides.rubyonrails.org/active_support_core_extensions.html#cherry-picking-a-definition
    
    Fixes #1685

--- lib/factory_bot.rb
+++ lib/factory_bot.rb
@@ -1,4 +1,5 @@
 require "set"
+require "active_support"
 require "active_support/core_ext/module/delegation"
 require "active_support/core_ext/module/attribute_accessors"
 require "active_support/deprecation"
