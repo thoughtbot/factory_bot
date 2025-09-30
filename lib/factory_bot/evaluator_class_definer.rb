242f8dc6660fd9948ac5e2f1c62e53e4fee1dda0
Valerie Burzynski <valerie.burzynski@thoughtbot.com>
2025-09-30 18:29:35 -0500

    refactor: make EvaluatorClassDefiner functional
    The work performed in FactoryBot::EvaluatorClassDefiner can be done
    without creating an instance of the class. It can be rewritten in a
    more functional manner with a module.

--- lib/factory_bot/evaluator_class_definer.rb
+++ lib/factory_bot/evaluator_class_definer.rb
@@ -1,19 +1,13 @@
 module FactoryBot
   # @api private
-  class EvaluatorClassDefiner
-    def initialize(attributes, parent_class)
-      @parent_class = parent_class
-      @attributes = attributes
-
-      attributes.each do |attribute|
-        evaluator_class.define_attribute(attribute.name, &attribute.to_proc)
-      end
-    end
-
-    def evaluator_class
-      @evaluator_class ||= Class.new(@parent_class).tap do |klass|
+  module EvaluatorClassDefiner
+    def self.define_evaluator_class(attributes, parent_class)
+      Class.new(parent_class).tap do |klass|
         klass.attribute_lists ||= []
-        klass.attribute_lists += [@attributes]
+        klass.attribute_lists += [attributes]
+        attributes.each do |attribute|
+          klass.define_attribute(attribute.name, &attribute.to_proc)
+        end
       end
     end
   end
